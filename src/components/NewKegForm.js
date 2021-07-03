847bae8b362e3c3246c85bbb6d3b28fa65a143f1
James Wyn <thisisjameswyn@gmail.com>
2021-07-03 09:13:40 -0700

    add basic create form

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewKegForm(props)
+{
+  return (
+    <React.Fragment>
+      <h3>Sample text</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewKegForm;
