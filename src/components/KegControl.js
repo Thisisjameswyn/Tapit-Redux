fd275cb1bb3bdc7a18aa07001b438ac5d0dbac7c
James Wyn <thisisjameswyn@gmail.com>
2021-07-06 04:58:50 -0700

    add keg change

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -9,7 +9,8 @@ class KegControl extends React.Component
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      masterKegList: []
+      masterKegList: [],
+      selectedKeg: null
     };
   }
 
@@ -24,6 +25,11 @@ class KegControl extends React.Component
   {
     const newMasterKegList = this.state.masterKegList.concat(newKeg);
     this.setState({ masterKegList: newMasterKegList, formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
   }
 
   render()
