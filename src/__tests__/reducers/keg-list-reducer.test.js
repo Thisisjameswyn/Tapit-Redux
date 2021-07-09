fbdf9a020a2334d11de9076eb5c1ab7f8e461be2
James Wyn <thisisjameswyn@gmail.com>
2021-07-09 11:05:42 -0700

    second test pass

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,6 +2,38 @@ import kegListReducer from "../../reducers/keg-list-reducer";
 
 describe("kegListReducer", () =>
 {
+  let action;
+  const kegData = {
+    name: "RPM",
+    brand: "Boneyard",
+    price: "5",
+    alcohol: "7.2",
+    id: 1
+  };
+
+  test("Should correctly add new keg data to masterKegList", () =>
+  {
+    const { name, brand, price, alcohol, id } = kegData;
+    action = {
+      type: "ADD_KEG",
+      name: name,
+      brand: brand,
+      price: price,
+      alcohol: alcohol,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcohol: alcohol,
+        id: id
+      }
+    })
+  })
+
   test("Should return default state if there is no action type passed into the reducer", () =>
   {
     expect(kegListReducer({}, { type: null })).toEqual({})
